094543e1cc26b1e8495c2e890d65ca18eaea9dc6
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-04 16:44:22 +0530

    add FlatCards.js

---
+++ components/FlatCards.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const FlatCards = () => {
+  return (
+    <View>
+      <Text>
+
+
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default FlatCards
