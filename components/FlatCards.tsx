254478c92555fa3af54375182cb280b70b0f0fae
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-04 17:14:39 +0530

    finish FlatCards

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -1,19 +1,53 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
 
 const FlatCards = () => {
   return (
     <View>
-      <Text>
-
-
-      </Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    
-})
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#5DA3FA',
+  },
+  cardThree: {
+    backgroundColor: '#50DBB4',
+  },
+});
 
-export default FlatCards
+export default FlatCards;
